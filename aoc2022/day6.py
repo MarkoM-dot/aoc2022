d03677ceae9c07e157864dc8e47a01abe9310c5e
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-12-11 18:05:24 +0100

    add(day6): init day 6

--- aoc2022/day6.py
+++ aoc2022/day6.py
@@ -1,5 +1,14 @@
+from pathlib import Path
 from solution_template import SolutionClass
 
 
 class DaySix(SolutionClass):
-    ...
+    @property
+    def data_path(self) -> Path:
+        return Path.cwd() / "data/year_2022_day_6.txt"
+
+    def part_one(self, data: str) -> str:
+        ...
+
+    def part_two(self, data: str) -> str:
+        ...
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -3,5 +3,25 @@ import unittest
 from aoc2022.day6 import DaySix
 
 
+part_one_data = (
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "7"),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", "5"),
+    ("nppdvjthqldpwncqszvftbrmjlhg", "6"),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "10"),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "11"),
+)
+
 class TestDaySix(unittest.TestCase):
-    ...
+    def setUp(self) -> None:
+        self.solver = DaySix
+
+    def test_part_one(self):
+        for input, expected in part_one_data:
+            with self.subTest(
+                "Testing peeking each stack rearranged by CrateMover 9000...",
+                input=input,
+                expected=expected,
+            ):
+                self.assertEqual(self.solver.part_one(input), expected)
+
+

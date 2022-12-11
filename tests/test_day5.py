0d647d9a393b405d65ff1f52de1d99d0c87eb1d6
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-12-11 16:46:20 +0100

    add(test5): add test to day 5

--- tests/test_day5.py
+++ tests/test_day5.py
@@ -15,6 +15,12 @@ drawing_to_stack_quantity = (
         3,
     ),
 )
+part_two_data = (
+    (
+        "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2\n",
+        "MCD",
+    ),
+)
 
 
 class TestDayFive(unittest.TestCase):
@@ -24,9 +30,17 @@ class TestDayFive(unittest.TestCase):
     def test_part_one(self):
         for input, expected in part_one_data:
             with self.subTest(
-                "Testing peeking each stack...", input=input, expected=expected
+                "Testing peeking each stack rearranged by CrateMover 9000...", input=input, expected=expected
             ):
                 self.assertEqual(self.solver.part_one(input), expected)
+
+    def test_part_two(self):
+        for input, expected in part_two_data:
+            with self.subTest(
+                "Testing peeking each stack rearranged by CrateMover9001...", input=input, expected=expected
+            ):
+                self.assertEqual(self.solver.part_two(input), expected)
+
 
     def test_get_stack_quantity(self):
         for input, expected in drawing_to_stack_quantity:

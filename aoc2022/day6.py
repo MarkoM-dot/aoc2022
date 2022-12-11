fdf39f122981329a50bcfcceea7338599fbf3ee4
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2022-12-11 18:59:14 +0100

    add(day6): add day 6 part 1 solution

--- aoc2022/day6.py
+++ aoc2022/day6.py
@@ -1,5 +1,8 @@
+from collections import deque
 from pathlib import Path
-from solution_template import SolutionClass
+from typing import Deque
+
+from .solution_template import SolutionClass
 
 
 class DaySix(SolutionClass):
@@ -7,8 +10,22 @@ class DaySix(SolutionClass):
     def data_path(self) -> Path:
         return Path.cwd() / "data/year_2022_day_6.txt"
 
+    def is_unique_substring(self, line: Deque[str]) -> bool:
+        return len(line) == len(set(line))
+
+    def find_start_of_packet_marker(self, line: str, sub_size: int = 4) -> int | None:
+        substring = deque(line[:sub_size])
+        marker = sub_size
+        while marker < len(line):
+            if self.is_unique_substring(substring):
+                return marker
+            substring.popleft()
+            substring.append(line[marker])
+            marker += 1
+        return None
+
     def part_one(self, data: str) -> str:
-        ...
+        return str(self.find_start_of_packet_marker(data))
 
     def part_two(self, data: str) -> str:
         ...
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -1,7 +1,7 @@
 import unittest
+from collections import deque
 
 from aoc2022.day6 import DaySix
-
 
 part_one_data = (
     ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", "7"),
@@ -11,17 +11,29 @@ part_one_data = (
     ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "11"),
 )
 
+deque_data = (
+    (deque(["A", "B", "C"]), True),
+    (deque(["A", "B", "A"]), False),
+    (deque(["q", "B", "t"]), True),
+)
+
+
 class TestDaySix(unittest.TestCase):
     def setUp(self) -> None:
-        self.solver = DaySix
+        self.solver = DaySix()
 
     def test_part_one(self):
         for input, expected in part_one_data:
             with self.subTest(
-                "Testing peeking each stack rearranged by CrateMover 9000...",
+                "Testing first start-of-packet marker...",
                 input=input,
                 expected=expected,
             ):
                 self.assertEqual(self.solver.part_one(input), expected)
 
-
+    def test_is_unique_substring(self):
+        for input, expected in deque_data:
+            with self.subTest(
+                "Testing unique sequence...", input=input, expected=expected
+            ):
+                self.assertEqual(self.solver.is_unique_substring(input), expected)

6c7c15bcdb4a379514dcd9ffaeeccc0ddcff6c40
MarkoM-dot <83985775+MarkoM-dot@users.noreply.github.com>
2023-02-04 10:16:20 +0100

    add(day9): solution and some tests for day 9

--- aoc2022/day9.py
+++ aoc2022/day9.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import math
 from pathlib import Path
 
@@ -18,7 +19,7 @@ class Vector:
     def __bool__(self):
         return bool(self.x or self.y)
 
-    def __eq__(self, other: "Vector"):
+    def __eq__(self, other: Vector):
         return tuple(self) == tuple(other)
 
     def __hash__(self):
@@ -27,45 +28,63 @@ class Vector:
     def __iter__(self):
         return (i for i in (self.x, self.y))
 
-    def __add__(self, other: "Vector"):
+    def __add__(self, other: Vector):
         return Vector(self.x + other.x, self.y + other.y)
 
-    def __radd__(self, other: "Vector"):
+    def __radd__(self, other: Vector):
         return self + other
 
     def __mul__(self, scalar: int):
         return Vector(self.x * scalar, self.y * scalar)
 
-    def is_touching(self, other: "Vector"):
-        return (
-            -1 <= (abs(self.x) - abs(other.x)) <= 1
-            and -1 <= (abs(self.y) - abs(other.y)) <= 1
-        )
+    def is_touching(self, other: Vector):
+        return abs(self.x - other.x) <= 1 and abs(self.y - other.y) <= 1
 
-    def follow(self, other: "Vector"):
+    def follow(self, other: Vector):
         """
         follow some other vector
         """
+        if other.y - self.y > 1:
+            self.y += 1
+            if other.x != self.x:
+                self.x = other.x
+        if other.y - self.y < -1:
+            self.y -= 1
+            if other.x != self.x:
+                self.x = other.x
+        if other.x - self.x > 1:
+            self.x += 1
+            if other.y != self.y:
+                self.y = other.y
+        if other.x - self.x < -1:
+            self.x -= 1
+            if other.y != self.y:
+                self.y = other.y
+        return self
+
+    @property
+    def coordinates(self) -> tuple[int, int]:
+        return self.x, self.y
 
 
 class Rope:
     def __init__(self) -> None:
         self.head = Vector()
         self.tail = Vector()
-        self.motions = {
+        self.motions: dict[str, Vector] = {
             "R": Vector(1, 0),
             "L": Vector(-1, 0),
             "U": Vector(0, 1),
             "D": Vector(0, -1),
         }
-        self.tail_visits = 1
+        self.tail_visits: set[tuple[int, int]] = {(0, 0),}
 
     def move(self, direction: str, steps: int):
         for _ in range(steps):
             self.head += self.motions[direction]
             if not self.tail.is_touching(self.head):
                 self.tail.follow(self.head)
-                self.tail_visits += 1
+                self.tail_visits.add(self.tail.coordinates)
 
 
 class DayNine(SolutionClass):
@@ -76,14 +95,14 @@ class DayNine(SolutionClass):
     def data_path(self) -> Path:
         return Path.cwd() / "data/year_2022_day_9.txt"
 
-    def part_one(self, data: str) -> int:
+    def part_one(self, data: str) -> str:
         for motion in (line.split() for line in data.splitlines()):
             match motion:
                 case [direction, steps]:
                     self.rope.move(direction, int(steps))
                 case _:
                     raise SyntaxError(f"Unrecognized syntax: {motion}")
-        return super().part_one(data)
+        return str(len(self.rope.tail_visits))
 
     def part_two(self, data: str) -> int:
         return super().part_two(data)
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -2,15 +2,15 @@ import unittest
 
 from aoc2022.day9 import DayNine, Rope, Vector
 
-part_one_data = (("R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2\n", "13"),)
+part_one_data = (
+        ("R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2\n", "13"),
+        ("R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2\nD 2\n", "14"),
+
+    )
 part_two_data = (("R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2\n", ""),)
 
 
 class TestDayNine(unittest.TestCase):
-    def setUp(self) -> None:
-        self.solver = DayNine()
-
-    @unittest.skip("not ready yet...")
     def test_part_one(self):
         for input, expected in part_one_data:
             with self.subTest(
@@ -18,7 +18,8 @@ class TestDayNine(unittest.TestCase):
                 input=input,
                 expected=expected,
             ):
-                self.assertEqual(self.solver.part_one(input), expected)
+                solver = DayNine()
+                self.assertEqual(solver.part_one(input), expected)
 
     @unittest.skip("Not ready yet...")
     def test_two_one(self):
@@ -28,7 +29,8 @@ class TestDayNine(unittest.TestCase):
                 input=input,
                 expected=expected,
             ):
-                self.assertEqual(self.solver.part_two(input), expected)
+                solver = DayNine()
+                self.assertEqual(solver.part_two(input), expected)
 
 
 class TestVector(unittest.TestCase):
@@ -52,12 +54,17 @@ class TestVector(unittest.TestCase):
     def test_vectors_can_follow(self):
         self.assertEqual(self.v.follow(self.w), Vector(0, 0))
 
-        moving_vec = self.v + Vector(1, 1)
+        moving_vec = self.v + Vector(1, 0)
         self.assertEqual(self.w.follow(moving_vec), Vector(0, 0))
 
-        moving_vec += Vector(1, 1)
+        moving_vec += Vector(0, 1)
+        self.assertEqual(self.w.follow(moving_vec), Vector(0, 0))
+
+        moving_vec += Vector(1, 0)
         self.assertEqual(self.w.follow(moving_vec), Vector(1, 1))
 
+        moving_vec += Vector(1, 0)
+        self.assertEqual(self.w.follow(moving_vec), Vector(2, 1))
 
 class TestRope(unittest.TestCase):
     def setUp(self) -> None:
@@ -69,3 +76,25 @@ class TestRope(unittest.TestCase):
 
         self.rope.move("U", 2)
         self.assertEqual(self.rope.head, Vector(3, 2))
+
+        self.rope.move("D", 8)
+        self.assertEqual(self.rope.head, Vector(3, -6))
+
+        self.rope.move("L", 2)
+        self.assertEqual(self.rope.head, Vector(1, -6))
+
+    def test_tail_visits(self):
+        self.rope.move("D", 10)
+        self.assertEqual(self.rope.head, Vector(0, -10))
+        self.assertEqual(self.rope.tail, Vector(0, -9))
+        self.assertEqual(len(self.rope.tail_visits), 10)
+
+        self.rope.move("L", 3)
+        self.assertEqual(self.rope.head, Vector(-3, -10))
+        self.assertEqual(self.rope.tail, Vector(-2, -10))
+        self.assertEqual(len(self.rope.tail_visits), 12)
+
+        self.rope.move("U", 5)
+        self.assertEqual(self.rope.head, Vector(-3, -5))
+        self.assertEqual(self.rope.tail, Vector(-3, -6))
+        self.assertEqual(len(self.rope.tail_visits), 16)
